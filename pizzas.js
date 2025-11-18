f093d2426761dc739998d346ea4ef1bce20f9774
Jagadish6246 <bjp.jagadish@outlook.com>
2025-11-18 22:02:51 +0530

    pizza cards ui

---
+++ pizzas.js
@@ -0,0 +1,75 @@
+var details = {
+  "restaurant_name": "The Pizza Palace",
+  "menu": {
+    "pizzas": [
+        {
+        "id": "nv003",
+        "name": "Chicken Pepperoni",
+        "type": "Non-Veg",
+        "description": "Classic chicken pepperoni with a generous amount of cheese.",
+        "prices": {
+          "Regular": 319.00,
+          "Medium": 579.00,
+          "Large": 839.00
+        }
+      },
+      {
+        "id": "v001",
+        "name": "Classic Margherita",
+        "type": "Veg",
+        "description": "A classic blend of mozzarella cheese and fresh basil.",
+        "prices": {
+          "Regular": 99.00,
+          "Medium": 199.00,
+          "Large": 399.00
+        }
+      },
+      {
+        "id": "v002",
+        "name": "Peppy Paneer",
+        "type": "Veg",
+        "description": "Flavorful paneer, crisp capsicum, and spicy red paprika.",
+        "prices": {
+          "Regular": 229.00,
+          "Medium": 399.00,
+          "Large": 599.00
+        }
+      },
+      {
+        "id": "nv001",
+        "name": "Pepper Barbecue Chicken",
+        "type": "Non-Veg",
+        "description": "Grilled chicken with a smoky and peppery barbecue flavor.",
+        "prices": {
+          "Regular": 229.00,
+          "Medium": 399.00,
+          "Large": 599.00
+        }
+      },
+      {
+        "id": "v003",
+        "name": "Veg Extravaganza",
+        "type": "Veg",
+        "description": "A hearty topping of black olives, onion, capsicum, mushroom, and corn.",
+        "prices": {
+          "Regular": 249.00,
+          "Medium": 459.00,
+          "Large": 699.00
+        }
+      },
+      
+      {
+        "id": "nv002",
+        "name": "Non Veg Supreme",
+        "type": "Non-Veg",
+        "description": "A meat feast with peri-peri chicken, grilled chicken rashers, and pepper barbecue chicken.",
+        "prices": {
+          "Regular": 319.00,
+          "Medium": 579.00,
+          "Large": 839.00
+        }
+      }
+      
+    ]
+  }
+}
